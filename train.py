acea488333b4c163d802e51e634e2f897c5a9733
vsvood <kaspirovich.vm@phystech.edu>
2022-08-30 17:30:21 +0300

    full interface
    full interface realization and little train logic fix

--- train.py
+++ train.py
@@ -7,12 +7,9 @@ import argparse
 from n_gram_model import NGramModel
 
 
-def extract_tokens(texts: list[str]) -> list[str]:
-    """List all the word tokens in texts."""
-    tokens = list[str]()
-    for text in texts:
-        tokens.extend(re.findall(r'\w+', text.lower()))
-    return tokens
+def extract_tokens(text: str) -> list[str]:
+    """List all the word tokens in text."""
+    return re.findall(r'\w+|\.', text.lower())
 
 
 def read(input_dir: str) -> list[str]:
@@ -28,16 +25,18 @@ def read(input_dir: str) -> list[str]:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input-dir', metavar='<input_dir>', dest='input_dir')
-    parser.add_argument('-m', '--model', metavar='<model_path>', dest='model_path')
+    parser.add_argument('--input-dir', '-i', metavar='<path>', dest='input_dir',
+                        help='Path to data dir. Data should be stored as normal text files')
+    parser.add_argument('--model', '-m', required=True, metavar='<path>', dest='model_path',
+                        help='Path to file where to save model')
     args = parser.parse_args()
 
     if args.input_dir:
         raw_data = read(args.input_dir)
     else:
-        raw_data = input()
+        raw_data = [input()]
 
-    data = tuple(extract_tokens(raw_data))
+    data = tuple(extract_tokens(''.join(raw_data)))
     model = NGramModel(2)
     model.fit(data)
 
