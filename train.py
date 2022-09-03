7feffb752d3d628d6392c984562ac795115fae2a
vsvood <kaspirovich.vm@phystech.edu>
2022-09-03 23:35:01 +0300

    trash model

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         raw_data = [input()]
 
     data = tuple(extract_tokens(''.join(raw_data)))
-    model = NGramModel(3)
+    model = NGramModel(2)
     model.fit(data)
 
     with open(args.model_path, 'wb') as file:
